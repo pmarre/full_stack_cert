7900bcc7df0bc356007b611bc3d6cdb04f2885ce
Patrick Marre <pmarre40@gmail.com>
2020-03-29 16:49:06 -0700

    remaining files

--- 03_interactive_frontend_development/API/ajax-test/main.js
+++ 03_interactive_frontend_development/API/ajax-test/main.js
@@ -1,9 +1,7 @@
-const baseURL = 'https://swapi.co/api/';
-
-function getData(type, cb) {
+function getData(url, cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', baseURL + type + '/');
+  xhr.open('GET', url);
   xhr.send();
 
   xhr.onreadystatechange = function() {
@@ -22,11 +20,25 @@ const getTableHeaders = obj => {
   return `<tr>${tableHeaders}</tr>`;
 };
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+  if (next && prev) {
+    return `<button onclick="writeToDocument('${prev}')">Previous</button><button onclick="writeToDocument('${next}')">Next</button>`;
+  } else if (next && !prev) {
+    return `<button onclick="writeToDocument('${next}')">Next</button>`;
+  } else if (!next && prev) {
+    return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+  }
+}
+
+function writeToDocument(url) {
   var tableRows = [];
   var el = document.getElementById('data');
   el.innerHTML = '';
-  getData(type, function(data) {
+  getData(url, function(data) {
+    var pagination;
+    if (data.next || data.previous) {
+      pagination = generatePaginationButtons(data.next, data.previous);
+    }
     data = data.results;
     var tableHeaders = getTableHeaders(data[0]);
 
