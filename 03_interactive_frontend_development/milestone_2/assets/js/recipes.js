26b933fbd0cf8ee63d688c40a1b8af9359b377af
Patrick Marre <pmarre40@gmail.com>
2020-04-16 10:23:01 -0700

    fixed styling on saved recipes page

--- 03_interactive_frontend_development/milestone_2/assets/js/recipes.js
+++ 03_interactive_frontend_development/milestone_2/assets/js/recipes.js
@@ -4,6 +4,54 @@ const RECIPE_API = '822ac61ec94b4490b4e562e53eccb278';
 const backupImage = './assets/images/rush-8.png';
 let savedRecipeIds = JSON.parse(window.localStorage.getItem('foodId'));
 
+// Build thumbnails
+const buildThumbnail = (response) => {
+  let recipes = response.results;
+  let savedRecipe = JSON.parse(window.localStorage.getItem('foodId'));
+  let img;
+  let thumbnailElement;
+  $('.recipe-card-list').prepend(`<div class="recipe-inner-container"></div>`);
+  $('#spinner').hide();
+
+  // Check if no images
+  // Set backup image if no images
+  console.log(response);
+
+  recipes.map((recipe) => {
+    if (recipe.imageUrls.length === 0) {
+      img = backupImage;
+    } else {
+      img = response.baseUri + recipe.image;
+    }
+    thumbnailElement = `
+    <div class="recipe-card">
+      <div class="img" style="background-image: url('${img}')">
+        <div class="save-btn-container">
+          <i class="far fa-heart heart-btn heart-btn-outline" id="heart-outline-${recipe.id}" style="display: block;" onclick="toggleLikeBtn(${recipe.id})"></i>
+          <i class="fas fa-heart heart-btn heart-btn-fill" style="display: none;" id="heart-fill-${recipe.id}" onclick="toggleLikeBtn(${recipe.id})"></i>
+        </div>
+      </div>
+      <div class="recipe-details">
+        <h3 class="recipe-card-heading">${recipe.title}</h3>
+        <span class="recipe-card-cook-time">Cook time: ${recipe.readyInMinutes} minutes</span>
+        <div class="btn-container">
+          <a class="btn recipe-card-view-recipe" href="#top-recipes" onclick="showIngredients(${recipe.id})">See recipe</a>
+        </div>
+      </div>
+    </div>`;
+
+    $('.recipe-inner-container').prepend(thumbnailElement);
+    if (savedRecipe.indexOf(recipe.id) !== -1) {
+      $(`#heart-fill-${recipe.id}`).toggle();
+      $(`#heart-outline-${recipe.id}`).toggle();
+      $(`#heart-fill-${recipe.id}`).addClass('saved');
+    }
+
+    if ($('.recipe-inner-container').length > 1) {
+      $('.recipe-inner-container').next().remove();
+    }
+  });
+};
 const showIngredients = (foodId) => {
   $('.recipe-card-list').css('display', 'none');
   $('.loading-container ').show();
@@ -24,34 +72,38 @@ const showIngredients = (foodId) => {
       $('.loading-container ').hide();
 
       // Put in own function or find a way to put in HTML
-      let el = `<div class="full-recipe-container">
-      <div id="${foodId}" class="banner-img" style="background-image: url('${img}')"></div>
+      let el = `
+      <div class="full-recipe-container">
+        <div id="${foodId}" class="banner-img" style="background-image: url('${img}')"></div>
         <div class="full-recipe-header">
-        <button class="btn back-btn" onclick="backBtn()">Back</button>
-        <div class="full-recipe-title">
-        <h2 class="full-recipe-heading">${response.title}</h2>
-        <div class="prep-icon-container">
-        <div class="prep-icon">
-        <i class="fas fa-utensils"></i>
-          <span>Serves ${response.servings}</span>
-        </div>
-        <div class="prep-icon">
-        <i class="far fa-clock"></i>
-          <span>${response.readyInMinutes} minutes</span>
-        </div>
-      </div>
-        </div>
-        <h4 class="summary-heading">Summary</h4>
-        <p class="recipe-summary">${response.summary}</p>
+          <button class="btn back-btn" onclick="backBtn()">Back</button>
+          <div class="full-recipe-title">
+            <h2 class="full-recipe-heading">${response.title}</h2>
+            <div class="prep-icon-container">
+              <div class="prep-icon">
+                <i class="fas fa-utensils"></i>
+                <span>Serves ${response.servings}</span>
+              </div>
+              <div class="prep-icon">
+                <i class="far fa-clock"></i>
+                <span>${response.readyInMinutes} minutes</span>
+              </div>
+            </div>
+          </div>
+          <h4 class="summary-heading">Summary</h4>
+          <p class="recipe-summary">${response.summary}</p>
         </div>
         <div>
-        <ul class="ingredient-list">
-        <h4>Ingredient List:</h4></ul>
+          <ul class="ingredient-list">
+          <h4>Ingredient List:</h4></ul>
         </div>
-        <div><ol class="instruction-list">
-        <h4>Cooking Instructions: </ol></div>
-        <h4 class="nutrition-heading">Nutrition Information:</h4>
-        <div id="donutchart" style="width: 100%; height: 400px;"></div>
+        <div>
+          <ol class="instruction-list">
+            <h4>Cooking Instructions: </h4>
+          </ol>
+        </div>
+          <h4 class="nutrition-heading">Nutrition Information:</h4>
+          <div id="donutchart" style="width: 100%; height: 400px;"></div>
       </div>`;
 
       // Is there a better way to do this? .full-recipes moved to bottom of div when recipe was added
@@ -86,50 +138,6 @@ $(document).ready(() => {
   // get & display random fact/recipe on page load
   getRandomFact();
   getRandomRecipe();
-
-  // Build thumbnails
-  const buildThumbnail = (response) => {
-    let recipes = response.results;
-    let savedRecipe = JSON.parse(window.localStorage.getItem('foodId'));
-    let img;
-    let thumbnailElement;
-    $('.recipe-card-list').prepend(
-      `<div class="recipe-inner-container"></div>`
-    );
-    $('#spinner').hide();
-
-    // Check if no images
-    // Set backup image if no images
-    recipes.map((recipe) => {
-      if (recipe.imageUrls.length === 0) {
-        img = backupImage;
-      } else {
-        img = response.baseUri + recipe.image;
-      }
-
-      thumbnailElement = `<div class="recipe-card">
-  <div class="img" style="background-image: url('${img}')"></div>
-  <div class="recipe-details">
-    <h3 class="recipe-card-heading">${recipe.title}</h3>
-    <span class="recipe-card-cook-time">Cook time: ${recipe.readyInMinutes} minutes</span>
-    <div class="btn-container"><a class="btn recipe-card-view-recipe" href="#top-recipes" onclick="showIngredients(${recipe.id})">See recipe</a></div>
-  </div>
-  <i class="far fa-heart heart-btn" id="heart-outline-${recipe.id}" style="display: block;" onclick="toggleLikeBtn(${recipe.id})"></i>
-  <i class="fas fa-heart heart-btn" style="display: none;" id="heart-fill-${recipe.id}" onclick="toggleLikeBtn(${recipe.id})"></i>
-</div>`;
-
-      $('.recipe-inner-container').prepend(thumbnailElement);
-      if (savedRecipe.indexOf(recipe.id) !== -1) {
-        $(`#heart-fill-${recipe.id}`).toggle();
-        $(`#heart-outline-${recipe.id}`).toggle();
-        $(`#heart-fill-${recipe.id}`).addClass('saved');
-      }
-    });
-
-    if ($('.recipe-inner-container').length > 1) {
-      $('.recipe-inner-container').next().remove();
-    }
-  };
 
   // On search, build thumbnails
 
@@ -234,6 +242,12 @@ function createChart(fat, protein, carbs) {
       colors: ['#3ab86f', '#05d5ff', '#5533ff'],
       chartArea: { width: '100%', height: '80%' },
       legend: { position: 'bottom' },
+      pieSliceText: 'value',
+      pieSliceTextStyle: {
+        color: '#fff',
+        fontName: 'Montserrat',
+        fontSize: 18,
+      },
     };
 
     var chart = new google.visualization.PieChart(
--- 03_interactive_frontend_development/milestone_2/assets/js/savedRecipes.js
+++ 03_interactive_frontend_development/milestone_2/assets/js/savedRecipes.js
@@ -3,6 +3,9 @@
 $(document).ready(() => {
   console.log(savedRecipeIds);
   let foodId;
+  $('.recipe-card-list').prepend(
+    `<div class="recipe-inner-container" id="saved-container"></div>`
+  );
   if (savedRecipeIds.length > 0) {
     foodId = savedRecipeIds.map((id) => {
       $.when(
@@ -11,9 +14,35 @@ $(document).ready(() => {
         )
       ).then((response) => {
         console.log(response);
-        $('#saved-container').prepend(
-          `<h3 id="${response.id} onclick="showIngredients(${response.id})")">${response.title}</h3><button class="btn" onclick="showIngredients(${response.id})">Test button</button>`
-        );
+
+        // buildThumbnail(response);
+        let thumbnailElement = `
+        <div class="recipe-card">
+            <div class="img" style="background-image: url('${response.image}')">
+                <div class="save-btn-container">
+                    <i class="far fa-heart heart-btn heart-btn-outline" id="heart-outline-${response.id}" style="display: block;" onclick="toggleLikeBtn(${response.id})"></i>
+                    <i class="fas fa-heart heart-btn heart-btn-fill" style="display: none;" id="heart-fill-${response.id}" onclick="toggleLikeBtn(${response.id})"></i>
+                </div>
+            </div>
+            <div class="recipe-details">
+                <h3 class="recipe-card-heading">${response.title}</h3>
+                <span class="recipe-card-cook-time">Cook time: ${response.readyInMinutes} minutes</span>
+                <div class="btn-container">
+                    <a class="btn recipe-card-view-recipe" href="#saved-recipes-container" onclick="showIngredients(${response.id})">See recipe</a>
+                </div>
+            </div>
+        </div>`;
+
+        $('#saved-container').prepend(thumbnailElement);
+        if (savedRecipeIds.indexOf(response.id) !== -1) {
+          $(`#heart-fill-${response.id}`).toggle();
+          $(`#heart-outline-${response.id}`).toggle();
+          $(`#heart-fill-${response.id}`).addClass('saved');
+        }
+
+        if ($('.recipe-inner-container').length > 1) {
+          $('.recipe-inner-container').next().remove();
+        }
       });
     });
   } else {
