44bb44b7c1bdabf5856a5840014c1872d625ed4a
Patrick Marre <pmarre40@gmail.com>
2020-05-11 16:41:27 -0700

    python lessons

--- 04_python/first_flask_app/run.py
+++ 04_python/first_flask_app/run.py
@@ -1,4 +1,5 @@
 import os
+import json
 # import flask class from Flask
 from flask import Flask, render_template, redirect, url_for, request
 
@@ -12,7 +13,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About")
+    data = []
+    with open("04_python/first_flask_app/data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
