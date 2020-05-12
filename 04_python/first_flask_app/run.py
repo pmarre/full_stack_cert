9e2757504144d671a31e7b30803b41f2636aa1db
Patrick Marre <pmarre40@gmail.com>
2020-05-12 15:47:51 -0700

    init

--- 04_python/first_flask_app/run.py
+++ 04_python/first_flask_app/run.py
@@ -1,9 +1,10 @@
 import os
 import json
 # import flask class from Flask
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 
 @app.route("/")
@@ -19,8 +20,23 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open("04_python/first_flask_app/data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
